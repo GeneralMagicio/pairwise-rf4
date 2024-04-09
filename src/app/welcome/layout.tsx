493d1e626f7a712034f143cec0e9f02619cc0c1f
alireza-sharifpour <ascode94@gmail.com>
2024-04-09 12:50:29 +0330

    add custom variables and layout

--- src/app/welcome/layout.tsx
+++ src/app/welcome/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const WelcomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className="centered-mobile-max-width">
       <div>Navbar</div>
       {children}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      maxWidth: {
+        mobile: "400px", // Adding the custom size variable 'mobile'
+      },
     },
   },
   plugins: [],
