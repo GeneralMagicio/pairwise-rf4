b934fd534a7f2fee64ada8ed90e899e1180ba7a1
alireza-sharifpour <ascode94@gmail.com>
2024-04-10 11:42:04 +0330

    update layout

--- src/app/welcome/layout.tsx
+++ src/app/welcome/layout.tsx
@@ -3,10 +3,14 @@ import Button from "../components/Button";
 
 const WelcomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="centered-mobile-max-width">
-      <div>Navbar</div>
-      {children}
-      <Button className="bg-primary w-full">Test Button</Button>
+    <div className="relative centered-mobile-max-width min-h-screen">
+      <div className="pb-16">{children}</div>
+      <div className="absolute bottom-0 w-full">
+        <div className="w-full border-t-2 border-gray-200"></div>
+        <div className="px-6 py-6">
+          <Button className="bg-primary w-full">Next</Button>
+        </div>
+      </div>
     </div>
   );
 };
