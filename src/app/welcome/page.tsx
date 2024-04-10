6abb920cd595bc2aad7d71ff86cc1daa1aebc990
alireza-sharifpour <ascode94@gmail.com>
2024-04-10 11:41:28 +0330

    add image and text

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
 import React from "react";
-import { json } from "stream/consumers";
 
 const WelcomePage = () => {
   return (
-    <div className="max-w-96 mx-auto">
-      <h1 className="text-2xl font-bold">Mobile Only Page</h1>
-      <p>This content is only visible on mobile devices.</p>
+    // Flex container to center content vertically and take at least the full viewport height
+    <div className="flex flex-col justify-center items-center">
+      <Image
+        src="/images/characters/welcome-character.png"
+        alt="Welcome character"
+        width={200}
+        height={200}
+      />
+      <h1 className="text-2xl font-bold mt-4">Welcome to Pairwise</h1>
     </div>
   );
 };
