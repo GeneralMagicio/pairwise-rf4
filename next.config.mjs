07864f55529454e848ab207d6bb1ad0ff6bef364
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 13:01:54 +0330

    update next config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'content.optimism.io',
+				port: '',
+				pathname: '/profile/v0/profile-image/10/**',
+			},
+		],
+	},
+};
 
 export default nextConfig;
