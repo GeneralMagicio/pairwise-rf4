f997aac2fe49b63d1d7d5764e319a3ef254345e0
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 15:45:05 +0330

    create category page

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,7 +1,55 @@
+'use client';
+
+import TopNavigation from '@/app/components/TopNavigation';
+import { useParams } from 'next/navigation';
 import React from 'react';
+import { Categories, projects } from '../mockData';
+import { Routes } from '@/app/constants/Routes';
+import CategoryBadge from '../components/CategoryBadge';
+import CategoryProjectItem from '../components/CategoryProjectItem';
 
 const CategoryPage = () => {
-	return <div>CategoryPage</div>;
+	const { categoryId } = useParams();
+	const selectedCategoryId =
+		typeof categoryId === 'string' ? categoryId : categoryId[0];
+	console.log(categoryId);
+	const selectedCategory = Categories.find(
+		category => category.id === +selectedCategoryId,
+	);
+	console.log('selected', selectedCategory);
+	const categoryProjects = projects.filter(
+		project => project.parentId === +selectedCategoryId,
+	);
+	console.log('projects', categoryProjects);
+	return (
+		<div>
+			<TopNavigation
+				link={Routes.Categories}
+				text={selectedCategory?.name}
+			/>
+			<div className='mt-6'>
+				<div className='mx-4 flex justify-between'>
+					<h1 className='text-xl font-bold'>
+						{selectedCategory?.name}
+					</h1>
+					<CategoryBadge />
+				</div>
+				<div className='mx-4 my-4'>
+					<p className='text-ph'>
+						{selectedCategory?.impactDescription}
+					</p>
+				</div>
+			</div>
+			<p className='mx-4 font-bold text-gray-600'>
+				Projects ({categoryProjects.length})
+			</p>
+			<div className='mt-2 '>
+				{categoryProjects.map(project => (
+					<CategoryProjectItem project={project} key={project.id} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default CategoryPage;
