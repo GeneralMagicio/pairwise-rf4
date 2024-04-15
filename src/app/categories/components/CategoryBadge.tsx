5fc80bbfa102f960d917ac4b8291cfb0cd659f23
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 15:45:14 +0330

    create category badge component

---
+++ src/app/categories/components/CategoryBadge.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryBadge = () => {
+	return (
+		<div className='whitespace-nowrap rounded-full bg-gray-300 px-2 py-1'>
+			Not ranked
+		</div>
+	);
+};
+
+export default CategoryBadge;
