bcfa56f9dc9f84c90810398e4a68053b143cdc09
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 11:12:00 +0330

    update style and use mockData

--- src/app/categories/components/CategoryItem.tsx
+++ src/app/categories/components/CategoryItem.tsx
@@ -1,19 +1,31 @@
-import Image from 'next/image'; // Make sure to install 'next/image'
+'use client';
 
-const CategoryItem = () => {
+import Image from 'next/image'; // Make sure to install 'next/image'
+import { ICategory } from '../types';
+import { useRouter } from 'next/navigation';
+import { Routes } from '@/app/constants/Routes';
+
+interface ICategoryProps {
+	category: ICategory;
+}
+
+const CategoryItem = ({ category }: ICategoryProps) => {
+	const router = useRouter();
+
 	return (
-		<div className='flex items-center justify-between gap-2 border-b border-b-gray-400 py-3'>
+		<div
+			className='flex cursor-pointer items-center justify-between gap-2 border-b border-b-gray-400 py-3'
+			onClick={() => router.push(`${Routes.Categories}/${category.id}`)}
+		>
 			<Image
 				src='/images/characters/welcome-character.png'
 				alt='Logo'
 				width={40}
 				height={40}
 			/>
-			<div>
-				<p className='font-bold'>Developer Ecosystem</p>
-				<p className='text-ph'>
-					Some description about the category here.
-				</p>
+			<div className='flex flex-1 flex-col'>
+				<p className='font-bold'>{category.name}</p>
+				<p className='text-ph'>{category.impactDescription}</p>
 			</div>
 			<div className='whitespace-nowrap rounded-full bg-gray-300 px-2 py-1'>
 				Not ranked
