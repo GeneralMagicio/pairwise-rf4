0f1f581df09cd1d1b7b035de8be95479612817a1
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 11:11:51 +0330

    use mockdata

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import CategoryItem from './components/CategoryItem';
+import { Categories } from './mockData';
 
 const page = () => {
 	return (
 		<div className='px-4'>
 			<h1 className='mt-6 text-2xl font-bold'>Categories</h1>
 			<p className='mb-6 text-ph '>Select one to begin ranking</p>
-			<CategoryItem />
-			<CategoryItem />
-			<CategoryItem />
+			{Categories.map(category => (
+				<CategoryItem key={category.id} category={category} />
+			))}
 		</div>
 	);
 };
