ac29c2d1a873f58e1dc469dbe83918b59dbd7c5b
alireza-sharifpour <ascode94@gmail.com>
2024-04-15 15:44:37 +0330

    add icon

---
+++ public/images/icons/IconArrowLeft.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const IconArrowLeft: React.FC = () => {
+	return (
+		<svg
+			width='20'
+			height='10'
+			viewBox='0 0 20 10'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M5 9L1 5M1 5L5 1M1 5L19 5'
+				stroke='#232634'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default IconArrowLeft;
