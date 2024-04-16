1269e0ab9ea96927cde3b2e2bc40135a86151e95
alireza-sharifpour <ascode94@gmail.com>
2024-04-16 14:47:25 +0330

    update Category page layout

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import TopNavigation from '@/app/components/TopNavigation';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 import { Categories, projects } from '../mockData';
 import { Routes } from '@/app/constants/Routes';
 import CategoryBadge from '../components/CategoryBadge';
 import CategoryProjectItem from '../components/CategoryProjectItem';
+import Button from '@/app/components/Button';
 
 const CategoryPage = () => {
+	const router = useRouter();
 	const { categoryId } = useParams();
 	const selectedCategoryId =
 		typeof categoryId === 'string' ? categoryId : categoryId[0];
@@ -21,32 +23,50 @@ const CategoryPage = () => {
 		project => project.parentId === +selectedCategoryId,
 	);
 	console.log('projects', categoryProjects);
+
 	return (
-		<div>
-			<TopNavigation
-				link={Routes.Categories}
-				text={selectedCategory?.name}
-			/>
-			<div className='mt-6'>
-				<div className='mx-4 flex justify-between'>
-					<h1 className='text-xl font-bold'>
-						{selectedCategory?.name}
-					</h1>
-					<CategoryBadge />
+		<div className='flex min-h-screen flex-col  justify-between'>
+			<div>
+				<TopNavigation
+					link={Routes.Categories}
+					text={selectedCategory?.name}
+				/>
+				<div className='mt-6'>
+					<div className='mx-4 flex justify-between'>
+						<h1 className='text-xl font-bold'>
+							{selectedCategory?.name}
+						</h1>
+						<CategoryBadge />
+					</div>
+					<div className='mx-4 my-4'>
+						<p className='text-ph'>
+							{selectedCategory?.impactDescription}
+						</p>
+					</div>
 				</div>
-				<div className='mx-4 my-4'>
-					<p className='text-ph'>
-						{selectedCategory?.impactDescription}
-					</p>
+				<p className='mx-4 font-bold text-gray-600'>
+					Projects ({categoryProjects.length})
+				</p>
+				<div className='mt-2 '>
+					{categoryProjects.map(project => (
+						<CategoryProjectItem
+							project={project}
+							key={project.id}
+						/>
+					))}
 				</div>
 			</div>
-			<p className='mx-4 font-bold text-gray-600'>
-				Projects ({categoryProjects.length})
-			</p>
-			<div className='mt-2 '>
-				{categoryProjects.map(project => (
-					<CategoryProjectItem project={project} key={project.id} />
-				))}
+			<div className='px-6 py-6'>
+				<Button
+					className='w-full bg-primary'
+					onClick={() =>
+						router.push(
+							`${Routes.Categories}/${categoryId}/pairwise`,
+						)
+					}
+				>
+					Start
+				</Button>
 			</div>
 		</div>
 	);
