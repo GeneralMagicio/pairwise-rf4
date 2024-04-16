2655c693859614385da11788cd8f3d982d2da247
alireza-sharifpour <ascode94@gmail.com>
2024-04-16 14:47:40 +0330

    create pairwise layout

---
+++ src/app/categories/[categoryId]/pairwise/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from 'react';
+
+const PairwiseLayout = ({ children }: { children: ReactNode }) => {
+	return <div>{children}</div>;
+};
+
+export default PairwiseLayout;
