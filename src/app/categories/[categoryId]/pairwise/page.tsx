df90894e885afdcd13d17572567d6fe3ed09b3f9
alireza-sharifpour <ascode94@gmail.com>
2024-04-16 15:45:17 +0330

    create CategoryPairwiseCard

--- src/app/categories/[categoryId]/pairwise/page.tsx
+++ src/app/categories/[categoryId]/pairwise/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import { Categories, projects } from '../../mockData';
 import { Routes } from '@/app/constants/Routes';
+import CategoryPairwiseCard from '../../components/CategoryPairwiseCard';
 
 const PariwisePage = () => {
 	const { categoryId } = useParams();
@@ -17,10 +18,15 @@ const PariwisePage = () => {
 		project => project.parentId === +selectedCategoryId,
 	);
 	return (
-		<div className='border-b border-b-gray-300 pb-7 pt-9'>
-			<div className='mx-4 flex justify-between gap-6'>
-				<p>{selectedCategory?.name}</p>
-				<Link href={`${Routes.Categories}/${categoryId}`}>✕</Link>
+		<div>
+			<div className='border-b border-b-gray-300 pb-7 pt-9'>
+				<div className='mx-4 flex justify-between gap-6'>
+					<p>{selectedCategory?.name}</p>
+					<Link href={`${Routes.Categories}/${categoryId}`}>✕</Link>
+				</div>
+			</div>
+			<div className='mt-7 flex justify-center'>
+				<CategoryPairwiseCard project={categoryProjects[0]} />
 			</div>
 		</div>
 	);
---
+++ src/app/categories/components/CategoryPairwiseCard.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import { IProject } from '../types';
+
+interface ICategoryPairwiseCardProps {
+	project: IProject;
+}
+
+const CategoryPairwiseCard = ({ project }: ICategoryPairwiseCardProps) => {
+	const truncate = (input: string) =>
+		input.length > 90 ? `${input.substring(0, 90)}...` : input;
+
+	return (
+		<div className='w-[324px] rounded-2xl px-3 pb-5 shadow-lg'>
+			<div className='mb-4'>
+				<Image
+					src={project.image ? project.image : ''}
+					alt={project.name}
+					width={300}
+					height={300}
+				/>
+			</div>
+			<p className='mb-4 font-bold'>{project.name}</p>
+			<p className='text-ph'>{truncate(project.impactDescription)}</p>
+		</div>
+	);
+};
+
+export default CategoryPairwiseCard;
