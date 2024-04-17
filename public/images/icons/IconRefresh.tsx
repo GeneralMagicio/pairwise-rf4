2efbca00f94d918131ddeab1a1b3bea24d7140c5
alireza-sharifpour <ascode94@gmail.com>
2024-04-17 13:27:16 +0330

    Add Icons

---
+++ public/images/icons/IconRefresh.tsx
@@ -0,0 +1,21 @@
+const IconRefresh = () => {
+	return (
+		<svg
+			width='20'
+			height='20'
+			viewBox='0 0 20 20'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M1.66669 8.33333C1.66669 8.33333 3.3375 6.05685 4.69488 4.69854C6.05226 3.34022 7.92802 2.5 10 2.5C14.1422 2.5 17.5 5.85786 17.5 10C17.5 14.1421 14.1422 17.5 10 17.5C6.58078 17.5 3.69595 15.2119 2.79316 12.0833M1.66669 8.33333V3.33333M1.66669 8.33333H6.66669'
+				stroke='#636779'
+				stroke-width='1.66667'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default IconRefresh;
