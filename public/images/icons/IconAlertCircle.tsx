9cffa560b0cfa534351c00fb23c78d03796943fd
alireza-sharifpour <ascode94@gmail.com>
2024-04-18 16:53:48 +0330

    add color to props

--- public/images/icons/IconAlertCircle.tsx
+++ public/images/icons/IconAlertCircle.tsx
@@ -1,4 +1,4 @@
-const IconAlertCircle = () => {
+const IconAlertCircle = ({ color = '#404454' }: IIconProps) => {
 	return (
 		<svg
 			width='22'
@@ -9,7 +9,7 @@ const IconAlertCircle = () => {
 		>
 			<path
 				d='M11 15V11M11 7H11.01M21 11C21 16.5228 16.5228 21 11 21C5.47715 21 1 16.5228 1 11C1 5.47715 5.47715 1 11 1C16.5228 1 21 5.47715 21 11Z'
-				stroke='#404454'
+				stroke={color}
 				stroke-width='2'
 				stroke-linecap='round'
 				stroke-linejoin='round'
