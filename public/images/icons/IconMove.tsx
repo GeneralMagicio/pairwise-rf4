6106dec142fce40771e7e5eb678599ea7dd74c72
alireza-sharifpour <ascode94@gmail.com>
2024-04-21 17:26:44 +0330

    add icon

---
+++ public/images/icons/IconMove.tsx
@@ -0,0 +1,28 @@
+const IconMove = () => {
+	return (
+		<svg
+			width='20'
+			height='20'
+			viewBox='0 0 20 20'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g clip-path='url(#clip0_2239_1272)'>
+				<path
+					d='M4.16667 7.49984L1.66667 9.99984M1.66667 9.99984L4.16667 12.4998M1.66667 9.99984H18.3333M7.5 4.1665L10 1.6665M10 1.6665L12.5 4.1665M10 1.6665V18.3332M12.5 15.8332L10 18.3332M10 18.3332L7.5 15.8332M15.8333 7.49984L18.3333 9.99984M18.3333 9.99984L15.8333 12.4998'
+					stroke='#E0E2EB'
+					stroke-width='2'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_2239_1272'>
+					<rect width='20' height='20' fill='white' />
+				</clipPath>
+			</defs>
+		</svg>
+	);
+};
+
+export default IconMove;
