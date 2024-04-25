4218dbeb658f32d4fe47bbbcf7cb9bff1c74190e
alireza-sharifpour <ascode94@gmail.com>
2024-04-25 15:48:45 +0330

    add walletConnect connector

--- src/app/providers/WagmiAppProvider.tsx
+++ src/app/providers/WagmiAppProvider.tsx
@@ -4,9 +4,17 @@ import React, { ReactNode } from 'react';
 import { WagmiProvider } from 'wagmi';
 import { http, createConfig } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
+import { walletConnect } from 'wagmi/connectors';
+
+export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID!;
 
 const config = createConfig({
 	chains: [mainnet, sepolia],
+	connectors: [
+		walletConnect({
+			projectId,
+		}),
+	],
 	transports: {
 		[mainnet.id]: http(),
 		[sepolia.id]: http(),
