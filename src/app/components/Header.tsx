8d5e6905bbc46c3710bc8ee2050f31a67c1c868e
alireza-sharifpour <ascode94@gmail.com>
2024-04-28 16:14:33 +0330

    add drawer to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,11 @@
 // components/Header.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image'; // Make sure to install 'next/image'
+import Drawer from './Drawer';
+import ConnectWalletContent from './ConnectWalletContent';
 
 const Header = () => {
+	const [isConnectDrawerOpen, setIsConnectDrawerOpen] = useState(false);
 	return (
 		<header className='sticky top-0 z-10 flex items-center justify-between border-b border-gray-300 bg-white p-4'>
 			<div className='flex items-center'>
@@ -13,9 +16,18 @@ const Header = () => {
 					height={40}
 				/>
 			</div>
-			<button className='rounded-full bg-primary px-4 py-2 text-sm text-white'>
+			<button
+				onClick={() => setIsConnectDrawerOpen(true)}
+				className='rounded-full bg-primary px-4 py-2 text-sm text-white'
+			>
 				Connect
 			</button>
+			<Drawer
+				setIsOpen={setIsConnectDrawerOpen}
+				isOpen={isConnectDrawerOpen}
+			>
+				<ConnectWalletContent />
+			</Drawer>
 		</header>
 	);
 };
