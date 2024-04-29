995710c1cff98d672833bad6fa43b3333f5f947a
alireza-sharifpour <ascode94@gmail.com>
2024-04-29 16:03:22 +0330

    add getCategories

---
+++ src/app/features/categories/getCategories.ts
@@ -0,0 +1,16 @@
+import { ICategory } from '@/app/categories/types';
+import { axios } from '@/lib/axios';
+import { useQuery } from '@tanstack/react-query';
+import { ExtractFnReturnType, QueryConfig } from '@/lib/react-query';
+import { AxiosResponse } from 'axios';
+
+export const getCategories = async (): Promise<AxiosResponse<ICategory[]>> => {
+	return axios.get('flow/collections');
+};
+
+export const useCategories = () => {
+	return useQuery({
+		queryKey: ['categories'],
+		queryFn: getCategories,
+	});
+};
