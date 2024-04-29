b3447ed0b85ccc7e009dd406af32774453e6bd9c
alireza-sharifpour <ascode94@gmail.com>
2024-04-29 17:16:01 +0330

    read categoriesdata from backend

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,13 +1,21 @@
+'use client';
+
 import React from 'react';
 import CategoryItem from './components/CategoryItem';
-import { Categories } from './mockData';
+import { useCategories } from '../features/categories/getCategories';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const CategoriesPage = () => {
+	const { data: categories, isLoading } = useCategories();
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
 	return (
 		<div className='px-4'>
 			<h1 className='mt-6 text-2xl font-bold'>Categories</h1>
 			<p className='mb-6 text-ph '>Select one to begin ranking</p>
-			{Categories.map(category => (
+			{categories?.data?.map(category => (
 				<CategoryItem key={category.id} category={category} />
 			))}
 		</div>
