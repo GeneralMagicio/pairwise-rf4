db7569290e03f525eaf4cb083b3d2960edc5cfc2
alireza-sharifpour <ascode94@gmail.com>
2024-05-01 12:21:32 +0330

    read CategoryPage data from backend

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -2,21 +2,19 @@
 
 import TopNavigation from '@/app/components/TopNavigation';
 import { useParams, useRouter } from 'next/navigation';
-import React from 'react';
-import { Categories, projects } from '../mockData';
+import React, { useState } from 'react';
 import { Routes } from '@/app/constants/Routes';
 import CategoryBadge from '../components/CategoryBadge';
 import CategoryProjectItem from '../components/CategoryProjectItem';
 import Button from '@/app/components/Button';
-
 import { useProjectsByCategoryId } from '@/app/features/categories/getProjectsByCategoryId';
-import { useCategories } from '@/app/features/categories/getCategories';
 import LoadingSpinner from '@/app/components/LoadingSpinner';
 import { useCategoryById } from '@/app/features/categories/getCategoryById';
 
 const CategoryPage = () => {
 	const router = useRouter();
 	const { categoryId } = useParams();
+
 	const selectedCategoryId =
 		typeof categoryId === 'string' ? categoryId : categoryId[0];
 	const { data: projects, isLoading: isProjectsLoading } =
@@ -26,7 +24,6 @@ const CategoryPage = () => {
 		useCategoryById(+selectedCategoryId);
 
 	const selectedCategory = data?.data?.collection;
-	console.log('Data', selectedCategory);
 
 	if (isProjectsLoading || isCategoryLoading) {
 		return <LoadingSpinner />;
@@ -55,12 +52,11 @@ const CategoryPage = () => {
 				<p className='mx-4 font-bold text-gray-600'>
 					Projects ({projects?.data?.length})
 				</p>
-				<div className='mt-2 '>
+				<div className='mt-2'>
 					{projects?.data?.map(project => (
-						<CategoryProjectItem
-							project={project}
-							key={project.id}
-						/>
+						<div key={project.id}>
+							<CategoryProjectItem project={project} />
+						</div>
 					))}
 				</div>
 			</div>
