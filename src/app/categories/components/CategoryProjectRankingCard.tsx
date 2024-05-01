e9f3354c53e1e2d3f2c98c9b97b59e94878c97b5
alireza-sharifpour <ascode94@gmail.com>
2024-05-01 12:21:54 +0330

    add drawer to CategoryProjectRankingCard

--- src/app/categories/components/CategoryProjectRankingCard.tsx
+++ src/app/categories/components/CategoryProjectRankingCard.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import { IProject } from '../types';
 import IconAlertCircle from 'public/images/icons/IconAlertCircle';
 import { truncate } from '@/app/helpers/text-helpers';
+import { useState } from 'react';
+import Drawer from '@/app/components/Drawer';
+import CategoriesProjectDrawerContent from './CategoriesProjectDrawerContent';
 
 interface ICategoryProjectRankingCardProps {
 	project: IProject;
@@ -10,6 +13,8 @@ interface ICategoryProjectRankingCardProps {
 const CategoryProjectRankingCard = ({
 	project,
 }: ICategoryProjectRankingCardProps) => {
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
 	return (
 		<div className='w-[324px] rounded-2xl px-3 pb-5 shadow-lg'>
 			<div className='mb-4'>
@@ -23,11 +28,17 @@ const CategoryProjectRankingCard = ({
 			</div>
 			<div className='flex justify-between'>
 				<p className='mb-4 font-bold'>{project.name}</p>
-				<div className='cursor-pointer'>
+				<div
+					className='cursor-pointer'
+					onClick={() => setIsDrawerOpen(true)}
+				>
 					<IconAlertCircle />
 				</div>
 			</div>
 			<p className='text-ph'>{truncate(project.impactDescription, 90)}</p>
+			<Drawer isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen}>
+				<CategoriesProjectDrawerContent project={project} />
+			</Drawer>
 		</div>
 	);
 };
