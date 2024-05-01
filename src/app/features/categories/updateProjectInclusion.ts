62297b02ee9903853c2f5c822a5b41b5861cbaad
alireza-sharifpour <ascode94@gmail.com>
2024-05-01 16:56:30 +0330

    add useUpdateProjectInclusion

---
+++ src/app/features/categories/updateProjectInclusion.ts
@@ -0,0 +1,31 @@
+import { InclusionState } from '@/app/categories/types';
+import { axios } from '@/lib/axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+type ProjectInclusionData = {
+	data: {
+		state: InclusionState;
+		id: number; //project id
+	};
+};
+
+export const updateProjectInclusion = ({ data }: ProjectInclusionData) => {
+	return axios.post('/flow/projects/set-inclusion', data);
+};
+
+export const useUpdateProjectInclusion = ({
+	categoryId,
+}: {
+	categoryId: number;
+}) => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: updateProjectInclusion,
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['projects', categoryId],
+			});
+		},
+	});
+};
