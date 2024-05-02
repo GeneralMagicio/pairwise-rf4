130a14382fead03062d97a7c627c18222e0d9d3c
alireza-sharifpour <ascode94@gmail.com>
2024-05-02 14:41:08 +0330

    add icons

--- public/images/icons/IconCheck.tsx
+++ public/images/icons/IconCheck.tsx
@@ -1,8 +1,8 @@
-const IconCheck = ({ color = 'white' }: IIconProps) => {
+const IconCheck = ({ color = 'white', size = '24' }: IIconProps) => {
 	return (
 		<svg
-			width='24'
-			height='24'
+			width={size}
+			height={size}
 			viewBox='0 0 24 24'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
--- public/images/icons/types.ts
+++ public/images/icons/types.ts
@@ -1,3 +1,4 @@
 interface IIconProps {
 	color?: string;
+	size?: string;
 }
