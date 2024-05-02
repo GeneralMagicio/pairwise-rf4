62bbf569ab0351371a00b2bf1b8abc4c07c99240
alireza-sharifpour <ascode94@gmail.com>
2024-05-02 16:48:16 +0330

    connect ProjectRankingSummaryPage to backend

--- src/app/categories/[categoryId]/project-ranking/summary/page.tsx
+++ src/app/categories/[categoryId]/project-ranking/summary/page.tsx
@@ -2,25 +2,40 @@
 
 import CategoryProjectItem from '@/app/categories/components/CategoryProjectItem';
 import { Categories, projects } from '@/app/categories/mockData';
+import { InclusionState } from '@/app/categories/types';
 import Button from '@/app/components/Button';
+import LoadingSpinner from '@/app/components/LoadingSpinner';
 import TopNavigation from '@/app/components/TopNavigation';
 import { Routes } from '@/app/constants/Routes';
+import { useCategoryById } from '@/app/features/categories/getCategoryById';
+import { useProjectsByCategoryId } from '@/app/features/categories/getProjectsByCategoryId';
 import { useParams, useRouter } from 'next/navigation';
 
 const ProjectRankingSummaryPage = () => {
-	const { categoryId } = useParams();
 	const router = useRouter();
+	const { categoryId } = useParams();
+
 	const selectedCategoryId =
 		typeof categoryId === 'string' ? categoryId : categoryId[0];
+	const { data: projects, isLoading: isProjectsLoading } =
+		useProjectsByCategoryId(+selectedCategoryId);
 
-	const selectedCategory = Categories.find(
-		category => category.id === +selectedCategoryId,
+	const { data, isLoading: isCategoryLoading } =
+		useCategoryById(+selectedCategoryId);
+
+	const selectedCategory = data?.data?.collection;
+
+	const includedProjects = projects?.data.filter(
+		project => project.inclusionState === InclusionState.Included,
 	);
 
-	const categoryProjects = projects.filter(
-		project => project.parentId === +selectedCategoryId,
+	const excludedProjects = projects?.data.filter(
+		project => project.inclusionState === InclusionState.Excluded,
 	);
 
+	if (isProjectsLoading || isCategoryLoading) {
+		return <LoadingSpinner />;
+	}
 	return (
 		<div className='flex min-h-[calc(100dvh)] flex-col  justify-between'>
 			<div>
@@ -33,17 +48,33 @@ const ProjectRankingSummaryPage = () => {
 				<div className='mx-4'>
 					<div className='mb-4 mt-6'>
 						<p className='font text-2xl font-bold'>
-							25 out of 38 projects selected
+							{includedProjects?.length || 0} out of{' '}
+							{projects?.data.length || 0} projects selected
 						</p>
 					</div>
 					<p className='mb-6 text-lg'>{selectedCategory?.name}</p>
-					<p className='text-lg'>Selected (25)</p>
-					{categoryProjects.map(project => (
+					<p className='text-lg font-semibold'>
+						Selected ({includedProjects?.length || 0})
+					</p>
+					{includedProjects?.map(project => (
 						<CategoryProjectItem
 							project={project}
 							key={project.id}
 						/>
 					))}
+					<div className='mt-6'>
+						<p className='mb-2 text-lg font-semibold'>
+							Selected ({excludedProjects?.length || 0})
+						</p>
+						{excludedProjects?.map(project => (
+							<div key={project.id} className='opacity-40'>
+								<CategoryProjectItem
+									project={project}
+									key={project.id}
+								/>
+							</div>
+						))}
+					</div>
 				</div>
 			</div>
 			<div className='sticky bottom-0 border-t border-b-gray-200 bg-white px-6 py-6'>
