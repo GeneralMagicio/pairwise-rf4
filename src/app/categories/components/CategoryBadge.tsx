3039049277b92594f1af1328d503e3ea9a20676c
alireza-sharifpour <ascode94@gmail.com>
2024-05-02 14:55:05 +0330

    add states to CategoryBadge component

--- src/app/categories/components/CategoryBadge.tsx
+++ src/app/categories/components/CategoryBadge.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { CollectionProgressStatus } from '../types';
+import IconCheck from 'public/images/icons/IconCheck';
 
-const CategoryBadge = () => {
-	return (
-		<div className='whitespace-nowrap rounded-full bg-gray-300 px-2 py-1'>
-			Not ranked
-		</div>
-	);
+const CategoryBadge = ({
+	progress = 'Pending',
+}: {
+	progress?: CollectionProgressStatus;
+}) => {
+	switch (progress) {
+		case 'Pending':
+			return (
+				<div className='whitespace-nowrap rounded-full border border-gray-300 bg-gray-100 px-2 py-1 text-ph'>
+					Not ranked
+				</div>
+			);
+		case 'Filtering':
+			return (
+				<div className='whitespace-nowrap rounded-full border border-gray-300 bg-gray-100 px-2 py-1 text-ph'>
+					Filtering
+				</div>
+			);
+		case 'Filtered':
+			return (
+				<div className='whitespace-nowrap rounded-full border border-blue-200 bg-blue-50 px-2 py-1 text-blue-700'>
+					Filtered
+				</div>
+			);
+		case 'WIP - Threshold':
+		case 'WIP':
+			return (
+				<div className='whitespace-nowrap rounded-full border border-gray-300 bg-gray-100 px-2 py-1 text-ph'>
+					Ranking
+				</div>
+			);
+		case 'Finished':
+		case 'Attested':
+			return (
+				<div className='flex items-center gap-1 whitespace-nowrap rounded-full border border-green-300 bg-green-50 px-2 py-1 text-green-600'>
+					<div>Ranked</div>
+					<IconCheck color='#16a34a' size='16' />
+				</div>
+			);
+
+		default:
+			return (
+				<div className='whitespace-nowrap rounded-full border border-green-300 bg-green-100 px-2 py-1 text-ph'>
+					Not ranked
+				</div>
+			);
+	}
 };
 
 export default CategoryBadge;
