082399f411748cbc279c7de3c30e8b0b9e8a23fe
alireza-sharifpour <ascode94@gmail.com>
2024-05-05 13:46:34 +0330

    created useUpdateCategoryMarkFiltered hook

---
+++ src/app/features/categories/updateCategoryMarkFiltered.ts
@@ -0,0 +1,32 @@
+import { InclusionState } from '@/app/categories/types';
+import { axios } from '@/lib/axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+type CategoryMarkFilteredData = {
+	data: {
+		cid: number; //Category ID
+	};
+};
+
+export const updateCategoryMarkFiltered = ({
+	data,
+}: CategoryMarkFilteredData) => {
+	return axios.post('/flow/mark-filtered', data);
+};
+
+export const useUpdateCategoryMarkFiltered = ({
+	categoryId,
+}: {
+	categoryId: number;
+}) => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: updateCategoryMarkFiltered,
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['category', categoryId],
+			});
+		},
+	});
+};
