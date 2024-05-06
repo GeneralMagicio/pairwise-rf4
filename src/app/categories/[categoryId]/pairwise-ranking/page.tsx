227d5658679916a1dab6f5b42e4f73fd190c5e5f
alireza-sharifpour <ascode94@gmail.com>
2024-05-06 17:11:31 +0330

    use useGetPairwisePairs data

--- src/app/categories/[categoryId]/pairwise-ranking/page.tsx
+++ src/app/categories/[categoryId]/pairwise-ranking/page.tsx
@@ -6,6 +6,9 @@ import { Categories, projects } from '../../mockData';
 import TopRouteIndicator from '@/app/components/TopRouteIndicator';
 import CategoryPairwiseCard from '../../components/CategoryPairwiseCard';
 import ProgressBar from '@/app/components/ProgressBar';
+import { useGetPairwisePairs } from '@/app/features/categories/getPairwisePairs';
+import LoadingSpinner from '@/app/components/LoadingSpinner';
+import { useCategoryById } from '@/app/features/categories/getCategoryById';
 
 const CategoryPairwiseRankingPage = () => {
 	const { categoryId } = useParams();
@@ -13,26 +16,31 @@ const CategoryPairwiseRankingPage = () => {
 	const selectedCategoryId =
 		typeof categoryId === 'string' ? categoryId : categoryId[0];
 
-	const selectedCategory = Categories.find(
-		category => category.id === +selectedCategoryId,
-	);
+	const { data: categoryData, isLoading: isCategoryLoading } =
+		useCategoryById(+selectedCategoryId);
 
-	const categoryProjects = projects.filter(
-		project => project.parentId === +selectedCategoryId,
-	);
+	const { data: pairwisePairs, isLoading: isPairwisePairsLoading } =
+		useGetPairwisePairs(+selectedCategoryId);
+
+	const [firstProject, secondProject] = pairwisePairs?.data.pairs[0] ?? [];
+
+	if (isCategoryLoading || isPairwisePairsLoading) {
+		return <LoadingSpinner />;
+	}
+
 	return (
 		<div>
-			<TopRouteIndicator name={selectedCategory?.name} />
+			<TopRouteIndicator name={categoryData?.data.collection.name} />
 			<div className='mb-10 mt-6 px-8'>
 				<ProgressBar progress={20} />
 				<p className='mt-2 text-sm'>20% of 100% Projects ranked</p>
 			</div>
 			<p className='text-bold mb-4 mt-6 px-3 text-center text-base'>
-				{`Which project should receive more RetroPGF funding in ${selectedCategory?.name}?`}
+				{`Which project should receive more RetroPGF funding in ${categoryData?.data.collection.name}?`}
 			</p>
 			<div className='flex flex-col items-center justify-center gap-3'>
-				<CategoryPairwiseCard project={categoryProjects[0]} />
-				<CategoryPairwiseCard project={categoryProjects[1]} />
+				<CategoryPairwiseCard project={firstProject} />
+				<CategoryPairwiseCard project={secondProject} />
 			</div>
 		</div>
 	);
