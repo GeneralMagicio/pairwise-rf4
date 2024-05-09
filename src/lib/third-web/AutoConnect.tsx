fad7c8e889b6c721c448ec4721097509a86779cd
alireza-sharifpour <ascode94@gmail.com>
2024-05-09 12:39:03 +0330

    fix removing queries issue

--- src/lib/third-web/AutoConnect.tsx
+++ src/lib/third-web/AutoConnect.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { client, smartWalletConfig } from './provider';
-import React, { ReactNode, useCallback, useContext, useEffect, useState } from 'react';
+import React, {
+	ReactNode,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
 import { WalletId, createWallet } from 'thirdweb/wallets';
 import { useActiveAccount, useActiveWallet, useConnect } from 'thirdweb/react';
 import { LAST_CONNECT_PERSONAL_WALLET_ID, activeChain } from './constants';
@@ -13,15 +19,25 @@ const AutoConnectContext = React.createContext<{
 	setIsAutoConnecting: Function;
 	loggedToPw: number;
 	setLoggedToPw: Function;
-}>({ isAutoConnecting: null, setIsAutoConnecting: () => {}, loggedToPw: 12345, setLoggedToPw: () => {} });
+}>({
+	isAutoConnecting: null,
+	setIsAutoConnecting: () => {},
+	loggedToPw: 12345,
+	setLoggedToPw: () => {},
+});
 
 export const AutoConnectProvider = ({ children }: { children: ReactNode }) => {
 	const [isAutoConnecting, setIsAutoConnecting] = useState(null);
-	const [loggedToPw, setLoggedToPw] = useState<number>(Date.now())
+	const [loggedToPw, setLoggedToPw] = useState<number>(Date.now());
 
 	return (
 		<AutoConnectContext.Provider
-			value={{ isAutoConnecting, setIsAutoConnecting, loggedToPw, setLoggedToPw }}
+			value={{
+				isAutoConnecting,
+				setIsAutoConnecting,
+				loggedToPw,
+				setLoggedToPw,
+			}}
 		>
 			{children}
 		</AutoConnectContext.Provider>
@@ -32,15 +48,16 @@ export const useIsAutoConnecting = () => {
 	const { isAutoConnecting, setIsAutoConnecting, loggedToPw, setLoggedToPw } =
 		useContext(AutoConnectContext);
 
-	return { isAutoConnecting, setIsAutoConnecting, loggedToPw, setLoggedToPw  };
+	return { isAutoConnecting, setIsAutoConnecting, loggedToPw, setLoggedToPw };
 };
 
 export const ThirdwebAutoConnect = () => {
 	const { connect } = useConnect();
 	const { replace, refresh } = useRouter();
-	const { isAutoConnecting, setIsAutoConnecting, setLoggedToPw } = useIsAutoConnecting();
+	const { isAutoConnecting, setIsAutoConnecting, setLoggedToPw } =
+		useIsAutoConnecting();
 	const account = useActiveAccount();
-	const wallet = useActiveWallet()
+	const wallet = useActiveWallet();
 
 	useEffect(() => {
 		const main = async () => {
@@ -67,26 +84,25 @@ export const ThirdwebAutoConnect = () => {
 		main();
 	}, [setIsAutoConnecting, connect]);
 
-
-
 	const checkLoginFlow = useCallback(async () => {
 		if (!wallet && isAutoConnecting === false) {
 			replace('/login');
 		} else if (account && wallet) {
 			const validToken = await isLoggedIn();
-			if (!validToken && alreadyInProgress === false) {	
+			if (!validToken && alreadyInProgress === false) {
 				await loginToPwBackend(
 					activeChain.id,
 					account.address,
 					account.signMessage,
 				);
-				setLoggedToPw(Date.now())
-				refresh();
+				setLoggedToPw(Date.now());
 			}
 		}
 	}, [account, wallet, isAutoConnecting, replace, refresh]);
-	
-	useEffect(() => {checkLoginFlow()}, [replace, wallet, isAutoConnecting, checkLoginFlow]);
+
+	useEffect(() => {
+		checkLoginFlow();
+	}, [replace, wallet, isAutoConnecting, checkLoginFlow]);
 
 	return <></>;
 };
