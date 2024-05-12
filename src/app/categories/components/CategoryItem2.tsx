94f875ccc4d58b1194c4f1563424c8e7cfc20412
Mahdi <mahdighajary@gmail.com>
2024-05-13 01:23:32 +0330

    Add similar comment and done pages for category ranking

---
+++ src/app/categories/components/CategoryItem2.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import Image from 'next/image'; // Make sure to install 'next/image'
+import { ICategory } from '../types';
+import { useRouter } from 'next/navigation';
+import { Routes } from '@/app/constants/Routes';
+import CategoryBadge from './CategoryBadge';
+import { truncate } from '@/app/helpers/text-helpers';
+
+interface ICategoryProps {
+	category: ICategory;
+}
+
+const CategoryItem = ({ category }: ICategoryProps) => {
+	const router = useRouter();
+
+	// const onCategoryClick = () => {
+	// 	switch (category.progress) {
+	// 		case 'Filtered':
+	// 			router.push(
+	// 				`${Routes.Categories}/${category.id}/project-ranking/summary`,
+	// 			);
+	// 			break;
+	// 		case 'Filtering':
+	// 			router.push(
+	// 				`${Routes.Categories}/${category.id}/project-ranking`,
+	// 			);
+	// 			break;
+	// 		case 'Finished':
+	// 		case 'Attested':
+	// 			router.push(
+	// 				`${Routes.Categories}/${category.id}/project-ranking/summary`,
+	// 			);
+	// 			break;
+	// 		case 'WIP':
+	// 		case 'WIP - Threshold':
+	// 			router.push(
+	// 				`${Routes.Categories}/${category.id}/pairwise-ranking`,
+	// 			);
+	// 			break;
+	// 		default:
+	// 			router.push(`${Routes.Categories}/${category.id}`);
+	// 	}
+	// };
+
+	return (
+		<div
+			className='flex cursor-pointer items-center justify-between gap-2 border-b border-b-gray-300 py-3 px-2'
+			// onClick={onCategoryClick}
+		>
+			<Image
+				className='rounded-full'
+				src={
+					category.image !== ''
+						? category.image
+						: '/images/characters/welcome-character.png'
+				}
+				alt='Logo'
+				width={40}
+				height={40}
+			/>
+			<div className='flex flex-1 flex-col'>
+				<p className='font-bold'>{category.name}</p>
+				<p className='text-ph'>
+					{truncate(category.impactDescription, 55)}
+				</p>
+			</div>
+			<CategoryBadge progress={"Attested"} />
+		</div>
+	);
+};
+
+export default CategoryItem;
