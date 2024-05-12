60f2f8a99167f53dd5bff47ca7e8f944d76d31c8
Mahdi <mahdighajary@gmail.com>
2024-05-12 18:15:01 +0330

    Add methods to get and update category pairwises

---
+++ src/app/features/categories/getCategoryPairs.ts
@@ -0,0 +1,16 @@
+import { axios } from '@/lib/axios';
+import { useQuery } from '@tanstack/react-query';
+import { IPairwisePairsResponse } from './getPairwisePairs';
+
+
+export const getCategoryPairs = async (): Promise<IPairwisePairsResponse['pairs'][0]> => {
+	const res = await axios.get(`flow/pairs`);
+  return res.data
+};
+
+export const useGetCategoryPairs = () => {
+	return useQuery({
+		queryKey: ['category-pairs'],
+		queryFn: () => getCategoryPairs(),
+	});
+};
