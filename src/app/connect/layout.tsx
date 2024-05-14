9acf84af39a01f6bee92f0d05e20995dcccbcae1
alireza-sharifpour <ascode94@gmail.com>
2024-05-14 14:51:20 +0330

    add connect page

---
+++ src/app/connect/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import ConnectHeader from './components/ConnectHeader';
+
+const ConnectLayout = () => {
+	return (
+		<div>
+			<ConnectHeader />
+		</div>
+	);
+};
+
+export default ConnectLayout;
---
+++ src/app/connect/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ConnectHomePage = () => {
+	return <div>ConnectHomePage</div>;
+};
+
+export default ConnectHomePage;
