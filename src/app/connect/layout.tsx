9d9a3b6efc370533a1e2b334775dae030e68a38d
alireza-sharifpour <ascode94@gmail.com>
2024-05-15 15:46:18 +0330

    add footer

--- src/app/connect/layout.tsx
+++ src/app/connect/layout.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 import Header from '../components/Header';
 import ConnectHeader from './components/ConnectHeader';
+import ConnectFooter from './components/ConnectFooter';
 
 const ConnectLayout = ({ children }: { children: ReactNode }) => {
 	return (
 		<div>
 			<ConnectHeader />
 			{children}
+			<ConnectFooter />
 		</div>
 	);
 };
