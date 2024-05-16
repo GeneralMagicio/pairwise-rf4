8f75974ac3dc503db2248719fe5fb3da16d1dedb
alireza-sharifpour <ascode94@gmail.com>
2024-05-16 15:01:06 +0330

    add badgesImages

---
+++ src/app/constants/BadgesData.ts
@@ -0,0 +1,18 @@
+export const badgesImages = [
+	{
+		src: '/images/badges/1.png',
+		alt: 'badge 1',
+	},
+	{
+		src: '/images/badges/2.png',
+		alt: 'badge 2',
+	},
+	{
+		src: '/images/badges/3.png',
+		alt: 'badge 3',
+	},
+	{
+		src: '/images/badges/4.png',
+		alt: 'badge 4',
+	},
+];
