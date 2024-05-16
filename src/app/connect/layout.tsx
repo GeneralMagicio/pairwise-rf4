59d41d1b3f0abcc0331dfa04ed7bf2637953d724
alireza-sharifpour <ascode94@gmail.com>
2024-05-16 14:59:05 +0330

    fixed styles

--- src/app/connect/layout.tsx
+++ src/app/connect/layout.tsx
@@ -1,13 +1,15 @@
+'use client';
+
 import React, { ReactNode } from 'react';
-import Header from '../components/Header';
+
 import ConnectHeader from './components/ConnectHeader';
 import ConnectFooter from './components/ConnectFooter';
 
 const ConnectLayout = ({ children }: { children: ReactNode }) => {
 	return (
-		<div>
+		<div className='flex min-h-[calc(100dvh)] flex-col'>
 			<ConnectHeader />
-			{children}
+			<div className='flex-grow'>{children}</div>
 			<ConnectFooter />
 		</div>
 	);
