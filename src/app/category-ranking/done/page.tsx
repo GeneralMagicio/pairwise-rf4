7b7cfc3c8fbb7890272de91798185bbfb0786caf
Mahdi <mahdighajary@gmail.com>
2024-05-21 16:17:30 +0330

    Small changes in the category ranking done page

--- src/app/category-ranking/done/page.tsx
+++ src/app/category-ranking/done/page.tsx
@@ -13,22 +13,21 @@ const CategoryRankingDone = () => {
 
 	return (
 		<div className='flex min-h-[calc(100dvh)] flex-col justify-between'>
-			<div className='mx-auto flex h-[80vh] flex-col items-center justify-center gap-4'>
+			<div className='mx-auto flex h-[80vh] max-w-[95%] flex-col items-center justify-center gap-4'>
 				<div className='inline-flex items-center justify-center rounded-full border-2 border-red-500 bg-red-200 p-2'>
 					<IconCheck color='red' />
 				</div>{' '}
-				<p className='mx-auto text-lg font-bold'>Vote submitted</p>
-				<p className='mx-auto text-ph'>
-					Now you can go back and vote on another category.
+				<p className='text-3xl font-bold'>Hooray!!</p>
+				<p className='self-start text-center text-xl text-ph'>
+					You have successfully submitted <br /> your vote.
+				</p>
+				<p className='self-start text-center text-ph'>
+					Now you can go back and vote on another <br />category.
 				</p>
 			</div>
 			<div className='border-t border-t-gray-300 px-6 py-6'>
 				<Button
-					onClick={() =>
-						router.push(
-							`${Routes.Categories}`,
-						)
-					}
+					onClick={() => router.push(`${Routes.Categories}`)}
 					className='w-full bg-primary'
 				>
 					Done
