66be3a016f49e493539c22bf6ece9c6172944284
Krati Jain <kratijain88@gmail.com>
2024-05-25 11:19:30 +0530

    fixed identity creation

--- src/app/components/CollectVotingPowerContent.tsx
+++ src/app/components/CollectVotingPowerContent.tsx
@@ -5,13 +5,19 @@ import { getMembersGroup, addMemberByApiKey, getGroup } from "../connect/anonvot
 import { getRoot } from "../connect/anonvote/utils/useSemaphore"
 import Button from './Button';
 import Image from 'next/image';
-import { useActiveAccount } from 'thirdweb/react';
 import { Identity } from "@semaphore-protocol/identity";
+import { generateProof } from "@semaphore-protocol/proof"
+import { Group } from "@semaphore-protocol/group"
 import IconCheck from 'public/images/icons/IconCheck';
 import { useSearchParams, useRouter } from "next/navigation"
-// import { useSigner } from '@/utils/eas';
+import supabase from "../connect/anonvote/utils/supabaseClient"
 import { useSignMessage } from 'wagmi'
-
+import {
+	encodeBytes32String,
+	toBigInt,
+	decodeBytes32String,
+	toBeHex
+} from "ethers"
 
 enum CollectVotingPowerState {
 	Not_Started,
@@ -30,145 +36,145 @@ const CollectVotingPowerContent = ({
 	setIsClaimDrawerOpen,
 }: ICollectionsVotingPowerContentProps) => {
 
-	const { data: signMessageData, error, isLoading, signMessage, variables } = useSignMessage()
-
 	const { address } = useAccount();
+	const { data: signMessageData, signMessage } = useSignMessage();
 	const router = useRouter();
 	const [_loading, setLoading] = useState<boolean>(false)
 
 	const [_identity, setIdentity] = useState<Identity>()
 	const [_isGroupMember, setIsGroupMember] = useState<boolean>(false)
-  	const [_users, setUsers] = useState<string[]>([])
+	const [_users, setUsers] = useState<string[] | null>([])
+	const [_feedback, setFeedback] = useState<string[]>([])
 
 	const [collectState, setCollectState] = useState(
 		CollectVotingPowerState.Not_Started,
 	);
 
-	const account = useActiveAccount();
-	// const signer = useSigner();
-
 	const createIdentity = async () => {
-		if (!account) return;
 
 		const message = `Sign this message to generate your Semaphore identity.`
-		// const signature = await signer.signMessage(message)
-		const signature = await signMessage({ message: message })
-		console.log("got the signature for semaphore identity: ", signature);
-		//   const signature = account.signMessage(message)
-		const identity = new Identity(signature)
-		console.log("identity.trapdoor: ",identity?.trapdoor);
-		console.log("identity.nullifier: ",identity?.nullifier);
-		console.log("identity.commitment: ",identity?.commitment);
+		await signMessage({ message: message })
+		console.log("got the signature for semaphore identity: ", signMessageData);
+		const identity = new Identity(signMessageData)
+		console.log("identity.trapdoor: ", identity?.trapdoor);
+		console.log("identity.nullifier: ", identity?.nullifier);
+		console.log("identity.commitment: ", identity?.commitment);
 		console.log("identity: ", identity);
+
 		setIdentity(identity)
+
+		console.log("_identity: ", _identity);
 		localStorage.setItem(localStorageTag, identity.toString())
 		console.log("Your new Semaphore identity was just created 🎉")
-	  }
 
-	  const getUsers = useCallback(async () => {
+	}
+
+	const getUsers = useCallback(async () => {
 		setLoading(true)
 		const users = await getMembersGroup(groupId)
-		setUsers(users)
+		if (users != null) {
+			setUsers(users)
+		}
 		setLoading(false)
 		return users
-	  }, [groupId])
+	}, [groupId])
 
-	  async function isMember() {
-		const users = await getUsers()
-		const answer = users?.includes(_identity!.commitment)
+	async function isMember() {
+		const answer = _users?.includes(_identity!.getCommitment().toString())
 		setIsGroupMember(answer || false)
-	  }
+	}
 
 	const joinGroup = async () => {
 		setLoading(true)
 		console.log("identity: ", _identity);
 		const commitment = _identity?.commitment
 		try {
-  			const apiKey = process.env.NEXT_PUBLIC_BANDADA_GROUP_API_KEY!
+			const apiKey = process.env.NEXT_PUBLIC_BANDADA_GROUP_API_KEY!
 			console.log("going to add the user in the anonymous group")
-   			await addMemberByApiKey(groupId, commitment, apiKey)
-    		const group = await getGroup(groupId)
-    		if (group) {
-      			const groupRoot = await getRoot(groupId, group.treeDepth, group.members)
-      			const { error } = await supabase
-        		.from("root_history")
-        		.insert([{ root: groupRoot.toString() }])
+			let commitment = _identity?.getCommitment().toString();
+			if (commitment) {
+				await addMemberByApiKey(groupId, commitment, apiKey)
+			}
+			const group = await getGroup(groupId)
+			if (group) {
+				const groupRoot = await getRoot(groupId, group.treeDepth, group.members)
+				const { error } = await supabase
+					.from("root_history")
+					.insert([{ root: groupRoot.toString() }])
 
-      			if (error) {
-        			console.error(error)
-					console.log(await response.json)
-        			return
-     			}
+				if (error) {
+					console.error(error)
+					return
+				}
 
 				setIsGroupMember(true)
 				const users = await getMembersGroup(groupId)
 				setUsers(users!.reverse())
-			} 
+			}
 		} finally {
 			setLoading(false)
 		}
-    }
-	
+	}
 
-	  //should be called before attestation is done
-	  const sendVote = async () => {
+	//should be called before attestation is done
+	const sendVote = async () => {
 		if (!_identity) {
 			console.log("unable to sendVote as identity is null")
-		  return
+			return
 		}
-	
+
 		const feedback = prompt("Please enter your vote:")
-	
+
 		const users = await getMembersGroup(groupId)
-	
+
 		if (feedback && users) {
-		  setLoading(true)
-	
-		  try {
-			const group = new Group(groupId, 16, users)
-	
-			const signal = toBigInt(encodeBytes32String(feedback)).toString()
-	
-			// this proof should be put inside the attestation
-			const { proof, merkleTreeRoot, nullifierHash } = await generateProof(
-			  _identity,
-			  group,
-			  groupId,
-			  signal
-			)
-	
-			const response = await fetch("api/send-feedback", {
-			  method: "POST",
-			  headers: { "Content-Type": "application/json" },
-			  body: JSON.stringify({
-				feedback: signal,
-				merkleTreeRoot,
-				nullifierHash,
-				proof
-			  })
-			})
-	
-			if (response.status === 200) {
-			  const data = await response.json()
-	
-			  console.log(data[0].signal)
-	
-			  if (data) setFeedback([data[0].signal, ..._feedback])
-	
-			  console.log(`Your vote was recorded 🎉`)
-			} else {
-			  console.log(await response.text())
-			  alert(await response.text())
+			setLoading(true)
+
+			try {
+				const group = new Group(groupId, 16, users)
+
+				const signal = toBigInt(encodeBytes32String(feedback)).toString()
+
+				// this proof should be put inside the attestation
+				const { proof, merkleTreeRoot, nullifierHash } = await generateProof(
+					_identity,
+					group,
+					groupId,
+					signal
+				)
+
+				const response = await fetch("api/send-feedback", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						feedback: signal,
+						merkleTreeRoot,
+						nullifierHash,
+						proof
+					})
+				})
+
+				if (response.status === 200) {
+					const data = await response.json()
+
+					console.log(data[0].signal)
+
+					if (data) setFeedback([data[0].signal, ..._feedback])
+
+					console.log(`Your vote was recorded 🎉`)
+				} else {
+					console.log(await response.text())
+					alert(await response.text())
+				}
+			} catch (error) {
+				console.error(error)
+
+				alert("Some error occurred in submitting vote, please try again!")
+			} finally {
+				setLoading(false)
 			}
-		  } catch (error) {
-			console.error(error)
-	
-			alert("Some error occurred in submitting vote, please try again!")
-		  } finally {
-			setLoading(false)
-		  }
 		}
-	  }
+	}
 
 	const handleCollect = async () => {
 		//Handle collect functionality here
@@ -176,18 +182,23 @@ const CollectVotingPowerContent = ({
 
 		// create bandada anonymous identity if not already present
 		await createIdentity();
-		setCollectState(CollectVotingPowerState.Collected);
-
 		//get users in the group
 		const users = await getMembersGroup(groupId)
-    	setUsers(users)
-	  	isMember()
+		if (users != null) {
+			setUsers(users)
+			console.log(users)
+		} else {
+			console.log("0 users in the group")
+		}
+		isMember()
 
-	  if (!_isGroupMember) {
-		await joinGroup()
-	  }
-	  await sendVote()
-	  
+		if (!_isGroupMember) {
+			console.log("joining the group as user is not already a member")
+			await joinGroup()
+		}
+		await sendVote()
+		setCollectState(CollectVotingPowerState.Collected);
+
 	};
 
 	switch (collectState) {
