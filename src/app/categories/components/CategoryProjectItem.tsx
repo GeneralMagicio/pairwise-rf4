5257898fa84806a84dadaafc966a6a8d791ca148
alireza-sharifpour <ascode94@gmail.com>
2024-05-26 13:35:43 +0330

    Fix CategoryProjectItem image placeholder

--- src/app/categories/components/CategoryProjectItem.tsx
+++ src/app/categories/components/CategoryProjectItem.tsx
@@ -9,17 +9,21 @@ const CategoryProjectItem = ({ project }: ICategoriesProjectItemProps) => {
 	return (
 		<div className='border-b border-b-gray-200 py-4'>
 			<div className='mx-4 flex items-center gap-2'>
-				<Image
-					className='rounded-full'
-					src={
-						project.image
-							? project.image
-							: '/images/characters/welcome-character.png'
-					}
-					alt='Logo'
-					width={40}
-					height={40}
-				/>
+				{project.image ? (
+					<Image
+						className='rounded-full'
+						src={project.image}
+						alt='Logo'
+						width={40}
+						height={40}
+					/>
+				) : (
+					<div className='relative h-[40px] w-[40px] rounded-full bg-gray-700'>
+						<p className='absolute inset-0 flex items-center justify-center overflow-hidden px-1 text-center text-[4px] text-white'>
+							{project.name}
+						</p>
+					</div>
+				)}
 				<p className='font-bold text-gray-700'>{project.name}</p>
 			</div>
 		</div>
