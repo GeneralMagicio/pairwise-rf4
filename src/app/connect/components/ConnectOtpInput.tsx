fd219e8be6f072ffa492d43960d5538430b3e0bb
alireza-sharifpour <ascode94@gmail.com>
2024-05-30 10:44:12 +0330

    add error to ConnectOTP component

--- src/app/connect/components/ConnectOtpInput.tsx
+++ src/app/connect/components/ConnectOtpInput.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/app/helpers/cn';
 import React, { FC, useEffect, useState } from 'react';
 import OTPInput from 'react-otp-input';
+import { ConnectErrorBox } from './ConnectErrorBox';
 
 export enum OtpState {
 	InProgress,
@@ -13,11 +14,11 @@ export enum OtpState {
 }
 interface Props {
 	onSubmit: () => void;
-	resend?: () => void;
 	otp: string;
 	setOtp: (otp: string) => void;
 	state: OtpState;
 	setState: (state: OtpState) => void;
+	setError: (error: string | false) => void;
 	error?: string | false;
 }
 
@@ -31,7 +32,8 @@ const ConnectOTPInput: FC<Props> = ({
 	setState,
 	onSubmit,
 	error,
-	resend,
+
+	setError,
 }) => {
 	const [resendTimer, setResendTimer] = useState(ResendTime);
 
@@ -50,17 +52,21 @@ const ConnectOTPInput: FC<Props> = ({
 
 	const handleOTPChange = (otp: string) => {
 		if (otp.length === OtpLength) setState(OtpState.Ready);
-		else setState(OtpState.InProgress);
+		else {
+			setState(OtpState.InProgress);
+			setError(false);
+		}
 		setOtp(otp);
 	};
 
 	return (
-		<div className='flex w-full items-center gap-4'>
+		<div className='flex w-full flex-col items-center gap-4'>
 			<OTPInput
 				inputStyle={cn(
 					`!w-12 h-12 mx-2 text-2xl rounded-lg border-2 border-gray-300`,
 					state === OtpState.Valid && 'border-success',
-					state === OtpState.Invalid && 'border-primary bg-pink-100',
+					state === OtpState.Invalid &&
+						'border-yellow-400 bg-yellow-50',
 				)}
 				numInputs={OtpLength}
 				onChange={handleOTPChange}
@@ -68,9 +74,8 @@ const ConnectOTPInput: FC<Props> = ({
 				inputType={'number'}
 				renderInput={props => <input {...props} />}
 				shouldAutoFocus
-				placeholder='0'
 			/>
-			{/* {error && <ErrorBox message={error} />} */}
+			{error && <ConnectErrorBox message={error} />}
 		</div>
 	);
 };
