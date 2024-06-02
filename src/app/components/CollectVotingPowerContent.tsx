f2b4043dc6f7df0b07ab8d77e0b12ec1cdc8c51d
alireza-sharifpour <ascode94@gmail.com>
2024-06-02 12:41:17 +0330

    remove redundant code

--- src/app/components/CollectVotingPowerContent.tsx
+++ src/app/components/CollectVotingPowerContent.tsx
@@ -1,14 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useAccount, useSignMessage } from 'wagmi';
+import React, { useState } from 'react';
+import { useAccount } from 'wagmi';
 import { formatAddress } from '../helpers/text-helpers';
-import { getMembersGroup, addMemberByApiKey, getGroup } from "../connect/anonvote/utils/bandadaApi"
-import { getRoot } from "../connect/anonvote/utils/useSemaphore"
 import Button from './Button';
 import Image from 'next/image';
-import { Identity } from "@semaphore-protocol/identity";
 import IconCheck from 'public/images/icons/IconCheck';
-import { useRouter } from "next/navigation"
-import supabase from "../connect/anonvote/utils/supabaseClient"
+
+import { useCreateIdentity } from '../hooks/useCreateIdentity';
 
 enum CollectVotingPowerState {
 	Not_Started,
@@ -20,68 +17,16 @@ interface ICollectionsVotingPowerContentProps {
 	setIsClaimDrawerOpen: (isOpen: boolean) => void;
 }
 
-const groupId = process.env.NEXT_PUBLIC_BANDADA_GROUP_ID!
-const localStorageTag = process.env.NEXT_PUBLIC_LOCAL_STORAGE_TAG!
-
 const CollectVotingPowerContent = ({
 	setIsClaimDrawerOpen,
 }: ICollectionsVotingPowerContentProps) => {
-
 	const { address } = useAccount();
-	const router = useRouter();
-	const [_loading, setLoading] = useState<boolean>(false)
 
-	const { signMessageAsync } = useSignMessage()
+	const { createIdentity } = useCreateIdentity();
+
 	const [collectState, setCollectState] = useState(
 		CollectVotingPowerState.Not_Started,
 	);
-
-	const createIdentity = async () => {
-
-		const message = `Sign this message to generate your Semaphore identity.`
-		const signature = await signMessageAsync({
-			message: message,
-		})
-		console.log("got the signature for semaphore identity: ", signature);
-		const identity = new Identity(signature)
-		console.log("identity.trapdoor: ", identity?.trapdoor);
-		console.log("identity.nullifier: ", identity?.nullifier);
-		console.log("identity.commitment: ", identity?.commitment);
-		console.log("identity: ", identity);
-		localStorage.setItem(localStorageTag, identity.toString())
-		console.log("Your new Semaphore identity was just created 🎉")
-		//get users in the group
-		const users = await getMembersGroup(groupId)
-		console.log("fetched users in the group: ", users?.length)
-
-		if (users == null || !users?.includes(identity!.getCommitment().toString())) {
-			console.log("joining the group as user is not already a member")
-			setLoading(true)
-			try {
-				const apiKey = process.env.NEXT_PUBLIC_BANDADA_GROUP_API_KEY!
-				console.log("going to add the user in the anonymous group")
-				let commitment = identity?.getCommitment().toString();
-				if (commitment) {
-					await addMemberByApiKey(groupId, commitment, apiKey)
-				}
-				const group = await getGroup(groupId)
-				if (group) {
-					const groupRoot = await getRoot(groupId, group.treeDepth, group.members)
-					const { error } = await supabase
-						.from("root_history")
-						.insert([{ root: groupRoot.toString() }])
-
-					if (error) {
-						console.error("error in getting group root: ", error)
-					}
-				}
-			} finally {
-				setLoading(false)
-			}
-		} else {
-			console.log("user is already in the group");
-		}
-	}
 
 	const handleCollect = async () => {
 		//Handle collect functionality here
@@ -90,7 +35,6 @@ const CollectVotingPowerContent = ({
 		// create bandada anonymous identity if not already present
 		await createIdentity();
 		setCollectState(CollectVotingPowerState.Collected);
-
 	};
 
 	switch (collectState) {
