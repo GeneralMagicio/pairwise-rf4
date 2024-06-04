638e2e5546a1cdf3abb9af4d90462c6d78e2fbe4
alireza-sharifpour <ascode94@gmail.com>
2024-06-04 14:13:12 +0330

    add icons

---
+++ public/images/icons/IconX.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const IconX = () => {
+	return (
+		<svg
+			width='17'
+			height='17'
+			viewBox='0 0 17 17'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g clip-path='url(#clip0_2266_7722)'>
+				<path
+					fill-rule='evenodd'
+					clip-rule='evenodd'
+					d='M11.508 15.8334L7.80843 10.5601L3.17697 15.8334H1.21758L6.93912 9.32081L1.21758 1.16675H6.24821L9.73505 6.13676L14.1039 1.16675H16.0633L10.6073 7.37774L16.5387 15.8334H11.508ZM13.6901 14.3468H12.3709L4.02314 2.65341H5.34247L8.68581 7.33552L9.26396 8.14799L13.6901 14.3468Z'
+					fill='#98A2B3'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_2266_7722'>
+					<rect
+						width='16'
+						height='16'
+						fill='white'
+						transform='translate(0.877731 0.5)'
+					/>
+				</clipPath>
+			</defs>
+		</svg>
+	);
+};
+
+export default IconX;
