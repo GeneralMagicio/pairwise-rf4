be1828373a5ced1bf9a4353c2f8147ced934579b
alireza-sharifpour <ascode94@gmail.com>
2024-06-12 12:37:58 +0330

    update CategoryPairwiseRanking page

--- src/app/categories/[categoryId]/pairwise-ranking/page.tsx
+++ src/app/categories/[categoryId]/pairwise-ranking/page.tsx
@@ -13,6 +13,15 @@ import { Routes } from '@/app/constants/Routes';
 import Button from '@/app/components/Button';
 import Modal from '@/app/components/Modal';
 import { useUpdatePairwiseFinish } from '@/app/features/categories/updatePairwiseFinish';
+import CategoryPairwiseCardWithMetrics from '../../components/CategoryPairwiseCardWithMetrics';
+import {
+	CategoryMetricData,
+	compareProjects,
+	ComparisonResult,
+	processProjectMetricsCSV,
+} from '@/utils/getMetrics';
+import { cn } from '@/app/helpers/cn';
+import { formatMetricsNumber } from '@/utils/numbers';
 
 interface IUserSeenRankingFinishedModal {
 	value: string;
@@ -24,6 +33,9 @@ const variants = {
 	visible: { opacity: 1 },
 };
 
+const project1Id = 'UF36qYsUzfwX6GDNuWgpDXrqsmrLcK4Rw5D4hZY5Pxc=';
+const project2Id = 'Q8XI5uvgwOT41A1eWKKVRoQdzjDvYXRtVLpT6n7KVrg=';
+
 const CategoryPairwiseRankingPage = () => {
 	const router = useRouter();
 	const { categoryId } = useParams();
@@ -32,6 +44,9 @@ const CategoryPairwiseRankingPage = () => {
 		typeof categoryId === 'string' ? categoryId : categoryId[0];
 
 	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const [formattedMetrics, setFormattedMetrics] =
+		useState<ComparisonResult>();
 
 	const { mutate, isPending: isVotingPending } = useUpdateProjectVote({
 		categoryId: +selectedCategoryId,
@@ -84,6 +99,27 @@ const CategoryPairwiseRankingPage = () => {
 			}),
 		);
 	};
+
+	const fetchMetrics = async () => {
+		try {
+			const response = await fetch('/data/cleaned_metrics.csv');
+			const data = await response.text();
+			const processedMap = processProjectMetricsCSV(data);
+			const formatted = compareProjects(
+				processedMap,
+				project1Id,
+				project2Id,
+			);
+			setFormattedMetrics(formatted);
+			console.log('compareProjects', formatted);
+		} catch (error) {
+			console.error('Failed to load or process CSV', error);
+		}
+	};
+
+	useEffect(() => {
+		fetchMetrics();
+	}, []);
 
 	useEffect(() => {
 		const hasUserSeenRankingFinishedModal = localStorage.getItem(
@@ -146,7 +182,7 @@ const CategoryPairwiseRankingPage = () => {
 				<p className='text-bold mb-4 mt-6 px-3 text-center text-base'>
 					{`Which project should receive more RetroPGF funding in ${categoryData?.data.collection.name}?`}
 				</p>
-				<div className='flex flex-col items-center justify-center gap-3'>
+				<div className='items-top flex justify-between gap-4 pb-6'>
 					<div
 						key={firstProject.id}
 						onClick={() => {
@@ -155,7 +191,9 @@ const CategoryPairwiseRankingPage = () => {
 						}}
 						className={`${isLoading ? 'cursor-not-allowed opacity-50' : 'opacity-100'} cursor-pointer`}
 					>
-						<CategoryPairwiseCard project={firstProject} />
+						<CategoryPairwiseCardWithMetrics
+							project={firstProject}
+						/>
 					</div>
 					<div
 						key={secondProject.id}
@@ -164,8 +202,75 @@ const CategoryPairwiseRankingPage = () => {
 						}
 						className={`${isLoading ? 'cursor-not-allowed opacity-50' : 'opacity-100'} cursor-pointer`}
 					>
-						<CategoryPairwiseCard project={secondProject} />
+						<CategoryPairwiseCardWithMetrics
+							project={secondProject}
+						/>
 					</div>
+				</div>
+				<div>
+					{formattedMetrics && (
+						<div>
+							{Object.keys(formattedMetrics).map(categoryKey => {
+								const categoryMetrics =
+									formattedMetrics[
+										categoryKey as keyof typeof formattedMetrics
+									];
+								return (
+									<div
+										className='my-2 flex flex-col p-3'
+										key={categoryKey}
+									>
+										<h3 className='pb-2 text-center font-bold'>
+											{categoryKey}
+										</h3>
+										{Object.keys(categoryMetrics).map(
+											metricKey => {
+												const metric =
+													categoryMetrics[
+														metricKey as keyof typeof categoryMetrics
+													];
+												return (
+													<div
+														key={metricKey}
+														className='flex justify-between gap-2 border-b-2 border-gray-200 py-2 last:border-b-0'
+													>
+														{/* <p className='font-medium'> */}
+														<p
+															className={cn(
+																metric.value1 >
+																	metric.value2
+																	? 'font-semibold text-green-600'
+																	: 'text-ph',
+															)}
+														>
+															{formatMetricsNumber(
+																metric.value1,
+															) ?? '--'}
+														</p>
+														<p className='flex-grow text-center text-ph'>
+															{metric.description}
+														</p>
+														<p
+															className={cn(
+																metric.value2 >
+																	metric.value1
+																	? 'font-semibold text-green-600'
+																	: 'text-ph',
+															)}
+														>
+															{formatMetricsNumber(
+																metric.value2,
+															) ?? '--'}
+														</p>
+													</div>
+												);
+											},
+										)}
+									</div>
+								);
+							})}
+						</div>
+					)}
 				</div>
 			</div>
 			{currentPercentage > thresholdPercentage ? (
