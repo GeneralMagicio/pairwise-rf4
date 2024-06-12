fbdfd5aeb4eb47f6a8eeb53ef273977b6cf2d1fa
alireza-sharifpour <ascode94@gmail.com>
2024-06-12 12:36:28 +0330

    add formatMetricsNumber

---
+++ src/utils/numbers.ts
@@ -0,0 +1,18 @@
+export function formatMetricsNumber(num: number) {
+	if (num === undefined || num === null) {
+		return undefined;
+	}
+
+	if (num >= 1000000) {
+		return (num / 1000000).toFixed(1) + 'M';
+	} else if (num >= 1000) {
+		return (num / 1000).toFixed(1) + 'K';
+	} else {
+		const decimalPlaces = (num.toString().split('.')[1] || []).length;
+		if (decimalPlaces > 3) {
+			return num.toFixed(3);
+		} else {
+			return num.toString();
+		}
+	}
+}
