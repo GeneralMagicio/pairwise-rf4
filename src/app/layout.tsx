cc8f2fe435eb44287bb03c42adc755713b5cf93c
alireza-sharifpour <ascode94@gmail.com>
2024-06-19 12:43:50 +0330

    add ConnectProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import './globals.css';
 import './globals.css';
 import { Thirdweb5Provider } from '@/lib/third-web/provider';
 import { AuthGuard } from '@/utils/AuthGuard';
+import { ConnectProvider } from './providers/ConnectProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,7 +28,9 @@ export default function RootLayout({
 					<TanstackProvider>
 						<Thirdweb5Provider>
 							<AuthGuard>
-								<div>{children}</div>
+								<ConnectProvider>
+									<div>{children}</div>
+								</ConnectProvider>
 							</AuthGuard>
 						</Thirdweb5Provider>
 					</TanstackProvider>
