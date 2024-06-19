9c870a2b37d8aa6916462baf4a92c043907ea201
alireza-sharifpour <ascode94@gmail.com>
2024-06-19 12:44:03 +0330

    add drawer to layout

--- src/app/categories/layout.tsx
+++ src/app/categories/layout.tsx
@@ -3,15 +3,38 @@
 import React, { ReactNode } from 'react';
 import Header from '../components/Header'; // Adjust the path as necessary
 import { useParams } from 'next/navigation';
+import CollectVotingPowerContent from '../components/CollectVotingPowerContent';
+import ConnectWalletContent from '../components/ConnectWalletContent';
+import Drawer from '../components/Drawer';
+import { useConnect } from '../providers/ConnectProvider';
 
 const CategoriesLayout = ({ children }: { children: ReactNode }) => {
 	const params = useParams();
+	const {
+		handleConnect,
+		isClaimDrawerOpen,
+		setIsClaimDrawerOpen,
+		isConnectDrawerOpen,
+		setIsConnectDrawerOpen,
+	} = useConnect();
+
 	console.log('params', params);
 
 	return (
 		<div className='centered-mobile-max-width'>
 			{!params.categoryId && <Header />}
 			{children}
+			<Drawer
+				setIsOpen={setIsConnectDrawerOpen}
+				isOpen={isConnectDrawerOpen}
+			>
+				<ConnectWalletContent onConnect={handleConnect} />
+			</Drawer>
+			<Drawer setIsOpen={setIsClaimDrawerOpen} isOpen={isClaimDrawerOpen}>
+				<CollectVotingPowerContent
+					setIsClaimDrawerOpen={setIsClaimDrawerOpen}
+				/>
+			</Drawer>
 		</div>
 	);
 };
