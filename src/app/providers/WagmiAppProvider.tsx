db32df9b265ecb994878a0515b426888efcac081
Mahdi <mahdighajary@gmail.com>
2024-06-24 12:31:39 +0330

    Add metamask to connectors

--- src/app/providers/WagmiAppProvider.tsx
+++ src/app/providers/WagmiAppProvider.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from 'react';
 import { WagmiProvider } from 'wagmi';
 import { http, createConfig } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
-import { walletConnect } from 'wagmi/connectors';
+import { walletConnect, metaMask } from 'wagmi/connectors';
 
 export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID!;
 
@@ -14,6 +14,7 @@ const config = createConfig({
 		walletConnect({
 			projectId,
 		}),
+		metaMask(),
 	],
 	transports: {
 		[mainnet.id]: http(),
