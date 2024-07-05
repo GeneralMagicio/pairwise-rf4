07cbc5ff315f4ee2bf29922b6f30ad6029ac9ead
Lovel George <lovelgeorge22@gmail.com>
2024-07-04 23:14:36 -0400

    toggle category view card

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -7,6 +7,8 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import { useGetCategoryPairs } from '../features/categories/getCategoryPairs';
 import { CategoryPairwiseModal } from '../category-ranking/components/CategoryPairwiseModal';
 import { useRouter } from 'next/navigation';
+import CategoryCardView from './components/CategoryCardView';
+import CategoryToggleButton from './components/CategoryToggleButton';
 
 const CategoriesPage = () => {
 	const [isModalOpen, setModalOpen] = useState(false);
@@ -14,6 +16,11 @@ const CategoriesPage = () => {
 	const { data: categoryPairs, isLoading: isGettingCategoryPairs } =
 		useGetCategoryPairs();
 	const router = useRouter();
+
+	const [isCardView, setIsCardView] = useState(() => {
+		const savedState = localStorage.getItem('isCardView');
+		return savedState !== null ? JSON.parse(savedState) : false;
+	});
 
 	useEffect(() => {
 		const bool = categoryPairs && categoryPairs.length > 0;
@@ -27,6 +34,11 @@ const CategoriesPage = () => {
 	if (isLoading) {
 		return <LoadingSpinner />;
 	}
+
+	const toggleView = () => {
+		localStorage.setItem('isCardView', JSON.stringify(!isCardView));
+		setIsCardView(!isCardView);
+	};
 	return (
 		<div className='px-4'>
 			<CategoryPairwiseModal
@@ -34,16 +46,32 @@ const CategoriesPage = () => {
 				close={closeModal}
 				handleSubmit={handleSubmit}
 			/>
-			<h1 className='mt-6 text-2xl font-bold'>Categories</h1>
-			<p className='mb-6 text-ph '>Select one to begin ranking</p>
-			{categories?.data?.map((category, index) => (
-				<CategoryItem
-					key={category.id}
-					category={category}
-					imageNumber={(index % 5) + 1}
-				/>
-			))}
-			<LogoutModal />
+			<div className='flex items-center justify-between'>
+				<div>
+					<h1 className='mt-6 text-2xl font-bold'>Categories</h1>
+					<p className='mb-6 text-ph '>Select one to begin ranking</p>
+				</div>
+				<CategoryToggleButton toggleView={toggleView} />
+			</div>
+			{isCardView ? (
+				<div className='grid grid-cols-2 gap-2'>
+					{categories?.data?.map((category, index) => (
+						<CategoryCardView
+							key={category.id}
+							category={category}
+							imageNumber={(index % 5) + 1}
+						/>
+					))}
+				</div>
+			) : (
+				categories?.data?.map((category, index) => (
+					<CategoryItem
+						key={category.id}
+						category={category}
+						imageNumber={(index % 5) + 1}
+					/>
+				))
+			)}
 		</div>
 	);
 };
