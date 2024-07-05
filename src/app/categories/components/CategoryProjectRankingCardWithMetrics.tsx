dc0920d561e9ad1a4197756e5fd841e5a9317446
Lovel George <lovelgeorge22@gmail.com>
2024-07-05 09:59:48 -0400

    made ui more resposive for xtra small screens

--- src/app/categories/components/CategoryProjectRankingCardWithMetrics.tsx
+++ src/app/categories/components/CategoryProjectRankingCardWithMetrics.tsx
@@ -75,7 +75,7 @@ const CategoryProjectRankingCardWithMetrics = ({
 			<div className='w-full select-none rounded-2xl pb-5'>
 				<div className='relative mb-4'>
 					{project.image ? (
-						<div className='mx-auto h-[360px] w-[360px]'>
+						<div className='xxs:w-[250px] xxs:h-[250px] xs:h-[360px] xs:w-[360px] mx-auto '>
 							<Image
 								src={project.image}
 								alt={project.name}
