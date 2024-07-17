d4da21036d07db3d0e353fad1c0ec0b63670bb91
Mahdi <mahdighajary@gmail.com>
2024-07-17 17:49:14 +0330

    End of the round

--- src/app/categories/components/Countdown.tsx
+++ src/app/categories/components/Countdown.tsx
@@ -1,71 +1,75 @@
-import { useEffect, useState } from 'react';
 
-function getTimeDifference(): {
-	days: number;
-	hours: number;
-	mins: number;
-	secs: number;
-} {
-	const now = new Date();
+// function getTimeDifference(): {
+// 	days: number;
+// 	hours: number;
+// 	mins: number;
+// 	secs: number;
+// } {
+// 	const now = new Date();
 
-	// Target date and time (July 16th at 20:00 CET)
-	const targetDate = new Date(
-		Date.UTC(
-			now.getFullYear(),
-			6,
-			17,
-			13 /* 15:55 CET adjusted to UTC */,
-			55,
-			0,
-		),
-	);
+// 	// Target date and time (July 16th at 20:00 CET)
+// 	const targetDate = new Date(
+// 		Date.UTC(
+// 			now.getFullYear(),
+// 			6,
+// 			17,
+// 			13 /* 15:55 CET adjusted to UTC */,
+// 			55,
+// 			0,
+// 		),
+// 	);
 
-	const diffMs = targetDate.getTime() - now.getTime();
+// 	const diffMs = targetDate.getTime() - now.getTime();
 
-	const dayMs = 1000 * 60 * 60 * 24;
-	const hourMs = dayMs / 24;
-	const minMs = hourMs / 60;
+// 	const dayMs = 1000 * 60 * 60 * 24;
+// 	const hourMs = dayMs / 24;
+// 	const minMs = hourMs / 60;
 
-	const days = Math.floor(diffMs / dayMs);
-	const hours = Math.floor((diffMs % dayMs) / hourMs);
-	const mins = Math.floor((diffMs % hourMs) / minMs);
-	const secs = Math.floor((diffMs % minMs) / 1000);
+// 	const days = Math.floor(diffMs / dayMs);
+// 	const hours = Math.floor((diffMs % dayMs) / hourMs);
+// 	const mins = Math.floor((diffMs % hourMs) / minMs);
+// 	const secs = Math.floor((diffMs % minMs) / 1000);
 
-	return {
-		days: Math.max(days, 0),
-		hours: Math.max(hours, 0),
-		mins: Math.max(mins, 0),
-		secs: Math.max(secs, 0),
-	};
-}
+// 	return {
+// 		days: Math.max(days, 0),
+// 		hours: Math.max(hours, 0),
+// 		mins: Math.max(mins, 0),
+// 		secs: Math.max(secs, 0),
+// 	};
+// }
 
-interface Time {
-	days: number;
-	hours: number;
-	mins: number;
-	secs: number;
-}
+// interface Time {
+// 	days: number;
+// 	hours: number;
+// 	mins: number;
+// 	secs: number;
+// }
 
 export const Countdown: React.FC = () => {
-	const [time, setTime] = useState<Time>(getTimeDifference());
+	// const [time, setTime] = useState<Time>(getTimeDifference());
 
-	useEffect(() => {
-		const interval = setInterval(
-			() => setTime(getTimeDifference()),
-			10 * 1000,
-		);
+	// useEffect(() => {
+	// 	const interval = setInterval(
+	// 		() => setTime(getTimeDifference()),
+	// 		10 * 1000,
+	// 	);
 
-		return () => clearInterval(interval);
-	}, []);
+	// 	return () => clearInterval(interval);
+	// }, []);
 
-	const { days, hours, mins } = time;
+	// const { days, hours, mins } = time;
 
-	return (
-		<div className='mt-4 flex h-16 w-full items-center justify-between bg-primary p-4 text-white'>
-			<p> Time left for voting </p>
-			<p className='text-xl font-bold'>
-				{`${days}d: ${hours}h: ${mins}min`}
-			</p>
+	// return (
+	// 	<div className='mt-4 flex h-16 w-full items-center justify-between bg-primary p-4 text-white'>
+	// 		<p> Time left for voting </p>
+	// 		<p className='text-xl font-bold'>
+	// 			{`${days}d: ${hours}h: ${mins}min`}
+	// 		</p>
+	// 	</div>
+	// );
+  	return (
+		<div className='mt-4 flex h-16 w-full items-center justify-center bg-primary p-4 text-white'>
+			<p className="text-lg"> Official Voting Has Ended. </p>
 		</div>
 	);
 };
